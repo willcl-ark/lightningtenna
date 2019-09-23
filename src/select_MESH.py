fb115e4d671e2a22a185fd6fa4dc999d919491da
willcl-ark <will8clark@gmail.com>
2019-09-23 13:49:06 +0100

    select_MESH and select_GATEWAY vaguely working
    still have occasional "Failed hdr decrypt"
    
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- src/select_MESH.py
+++ src/select_MESH.py
@@ -1,4 +1,3 @@
-import base58
 import select
 import socket
 import queue
@@ -12,6 +11,7 @@ LOCAL_IP = "127.0.0.1"
 LOCAL_PORT = 9733
 REMOTE_IP = "77.98.116.8"
 REMOTE_PORT = 9733
+RECV_SIZE = 210
 
 ART_DELAY = 13
 
@@ -30,7 +30,8 @@ c.set_geo_region(int(CONFIG["gotenna"]["GEO_REGION"]))
 def mesh_auto_send(conn):
     while True:
         if not conn.events.send_via_mesh.empty():
-            conn.send_broadcast(base58.b58encode_check(conn.events.send_via_mesh.get()).decode())
+            data = conn.events.send_via_mesh.get()
+            conn.send_broadcast(data, binary=True)
         else:
             time.sleep(0.5)
 
@@ -38,68 +39,52 @@ def mesh_auto_send(conn):
 mesh_send_thread = threading.Thread(target=mesh_auto_send, args=[c])
 mesh_send_thread.start()
 
+
 inputs = []
 outputs = []
 message_queues = {}
-send_times = {}
 
 # Server setup -- will accept new connection "local" and add it to select
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setblocking(False)
 server.bind((LOCAL_IP, LOCAL_PORT))
 server.listen(5)
-# hack to wait for local connection
+# hack to force waiting for local C-Lightning connection
 local = None
 while True:
     try:
         local, client_address = server.accept()
         local.setblocking(False)
         inputs.append(local)
-        message_queues[local] = queue.Queue()
-        send_times[local] = []
+        outputs.append(local)
+        message_queues[local] = c.events.send_via_socket
         break
     except BlockingIOError:
         time.sleep(1)
         pass
 
 
-# remote setup -- will create an outbound socket
-remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-remote.connect((REMOTE_IP, REMOTE_PORT))
-inputs.append(remote)
-outputs.append(remote)
-message_queues[remote] = queue.Queue()
-send_times[remote] = []
+# # remote setup -- will create an outbound socket
+# remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# remote.connect((REMOTE_IP, REMOTE_PORT))
+# inputs.append(remote)
+# outputs.append(remote)
+# message_queues[remote] = queue.Queue()
 
 
 while inputs:
     readable, writable, exceptional = select.select(inputs, outputs, inputs)
     for s in readable:
-        if s is server:
-            local, client_address = s.accept()
-            local.setblocking(0)
-            inputs.append(local)
-            message_queues[local] = queue.Queue()
+        data = s.recv(RECV_SIZE)
+        if data:
+            print(f"\nRead {naturalsize(len(data))} data from {s.getsockname()}:")
+            hexdump(data)
+            if s is local:
+                # add received C-Lightning socket msgs to mesh queue
+                c.events.send_via_mesh.put(data)
         else:
-            data = s.recv(210)
-            if data:
-                print(f"\nRead {naturalsize(len(data))} data from {s.getsockname()}:")
-                hexdump(data)
-                if s is local:
-                    message_queues[remote].put(data)
-                    c.events.send_via_mesh.put(data)
-                else:
-                    message_queues[local].put(data)
-                if s not in outputs:
-                    outputs.append(s)
-            else:
-                pass
-                # if s in outputs:
-                #     outputs.remove(s)
-                # inputs.remove(s)
-                # print(f"Closing {s.getsockname()}")
-                # s.close()
-                # del message_queues[s]
+            # don't close the socket!
+            pass
 
     for s in writable:
         try:
@@ -109,13 +94,6 @@ while inputs:
             # outputs.remove(s)
         else:
             print(f"Sending {naturalsize(len(next_msg))} data to {s.getsockname()}\n")
-            # delay hack:
-            if len(send_times[s]) == 0:
-                pass
-            else:
-                wait = int((send_times[s][-1] + ART_DELAY) - time.time()) + 1
-                print_timer(wait)
-            send_times[s].append(time.time())
             s.send(next_msg)
 
     for s in exceptional:
