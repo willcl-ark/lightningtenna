d5fe3c6e9ea4b08b55735bdda6fd2f0f272b485f
willcl-ark <will8clark@gmail.com>
2019-09-30 13:44:19 +0100

    Use a random GID if not supplied in config.ini
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lightningtenna/gotenna_connections.py
+++ lightningtenna/gotenna_connections.py
@@ -1,3 +1,5 @@
+import random
+
 from config import CONFIG
 from connection import Connection
 
@@ -6,9 +8,17 @@ def setup_gotenna_conn(name, server, send_to_trio, receive_from_trio):
     conn = Connection(name, send_to_trio, receive_from_trio)
     conn.sdk_token(CONFIG["gotenna"]["SDK_TOKEN"])
     if server:
-        conn.set_gid(int(CONFIG.get("gotenna", "DEBUG_GID", fallback=1234567)))
+        conn.set_gid(
+            int(
+                CONFIG.get(
+                    "gotenna", "DEBUG_GID", fallback=random.randint(1, 999999999)
+                )
+            )
+        )
     else:
-        conn.set_gid(int(CONFIG.get("gotenna", "GID", fallback=9876543)))
+        conn.set_gid(
+            int(CONFIG.get("gotenna", "GID", fallback=random.randint(1, 999999999)))
+        )
 
     conn.set_geo_region(int(CONFIG.get("gotenna", "GEO_REGION", fallback=1)))
     return conn
